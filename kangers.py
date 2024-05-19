83006dd145126f3030ad539a17ea95066546c3f8
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-05-19 19:48:42 +0530

    Delete kangers.py

--- kangers.py
+++
@@ -1,2 +0,0 @@
-## DON'T WORRY KANGERS YOU GOT YOUR POSITION
-1. 6507008495 || https://t.me/WHO182 || https://github.com/cute-us || https://github.com/hinata-exe || 6337221543 || https://t.me/nexown || https://www.instagram.com/pvtt.qn || hinata-exe.github.io || https://t.me/nex_management || -1002068576120
