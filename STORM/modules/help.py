fab8efbcfa67276a9816ea85f2142c774c329907
ᴋᴜɴᴀʟ <155554911+VARC9210@users.noreply.github.com>
2024-07-04 18:56:46 +0530

    3.1.x

--- STORM/modules/help.py
+++ STORM/modules/help.py
@@ -22,7 +22,7 @@
 
 from telethon import events, Button
 
-from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
+from config import KEX1, KEX2, KEX3, KEX4, KEX5, KEX6, KEX7, KEX8, KEX9, KEX10, SUDO_USERS, CMD_HNDLR as hl
 
 
 HELP_STRING = f"""
@@ -50,16 +50,7 @@ HELP_BUTTON = [
   ]
 
 
-@X1.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X2.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X3.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X4.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X5.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X6.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X7.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X8.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X9.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
-@X10.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
+@KEX1.on(events.NewMessage(incoming=True, pattern=r"\%shelp(?: |$)(.*)" % hl))
 async def help(event):
     if event.sender_id in SUDO_USERS:
         try:
@@ -207,16 +198,7 @@ spam_msg = f"""
 ** © @KKEX_XD**
 """                                
            
-@X1.on(events.CallbackQuery(pattern=r"help_back"))
-@X2.on(events.CallbackQuery(pattern=r"help_back"))
-@X3.on(events.CallbackQuery(pattern=r"help_back"))
-@X4.on(events.CallbackQuery(pattern=r"help_back"))
-@X5.on(events.CallbackQuery(pattern=r"help_back"))
-@X6.on(events.CallbackQuery(pattern=r"help_back"))
-@X7.on(events.CallbackQuery(pattern=r"help_back"))
-@X8.on(events.CallbackQuery(pattern=r"help_back"))
-@X9.on(events.CallbackQuery(pattern=r"help_back"))
-@X10.on(events.CallbackQuery(pattern=r"help_back"))
+@KEX1.on(events.CallbackQuery(pattern=r"help_back"))
 async def helpback(event):
     if event.query.user_id in SUDO_USERS:    
         await event.edit(
@@ -239,16 +221,7 @@ async def helpback(event):
         await event.answer("ɴᴏᴏʙ ! ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ꜱᴛᴏʀᴍ ꜱᴘᴀᴍ ʙᴏᴛꜱ !! @KKEX_XD", cache_time=0, alert=True)
 
 
-@X1.on(events.CallbackQuery(pattern=r"spam"))
-@X2.on(events.CallbackQuery(pattern=r"spam"))
-@X3.on(events.CallbackQuery(pattern=r"spam"))
-@X4.on(events.CallbackQuery(pattern=r"spam"))
-@X5.on(events.CallbackQuery(pattern=r"spam"))
-@X6.on(events.CallbackQuery(pattern=r"spam"))
-@X7.on(events.CallbackQuery(pattern=r"spam"))
-@X8.on(events.CallbackQuery(pattern=r"spam"))
-@X9.on(events.CallbackQuery(pattern=r"spam"))
-@X10.on(events.CallbackQuery(pattern=r"spam"))
+@KEX1.on(events.CallbackQuery(pattern=r"spam"))
 async def help_spam(event):
     if event.query.user_id in SUDO_USERS:    
         await event.edit(spam_msg,
@@ -258,16 +231,7 @@ async def help_spam(event):
         await event.answer("ɴᴏᴏʙ ! ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ꜱᴛᴏʀᴍ ꜱᴘᴀᴍ ʙᴏᴛꜱ !! @KKEX_XD", cache_time=0, alert=True)
 
 
-@X1.on(events.CallbackQuery(pattern=r"raid"))
-@X2.on(events.CallbackQuery(pattern=r"raid"))
-@X3.on(events.CallbackQuery(pattern=r"raid"))
-@X4.on(events.CallbackQuery(pattern=r"raid"))
-@X5.on(events.CallbackQuery(pattern=r"raid"))
-@X6.on(events.CallbackQuery(pattern=r"raid"))
-@X7.on(events.CallbackQuery(pattern=r"raid"))
-@X8.on(events.CallbackQuery(pattern=r"raid"))
-@X9.on(events.CallbackQuery(pattern=r"raid"))
-@X10.on(events.CallbackQuery(pattern=r"raid"))
+@KEX1.on(events.CallbackQuery(pattern=r"raid"))
 async def help_raid(event):
     if event.query.user_id in SUDO_USERS:
         await event.edit(raid_msg,
@@ -277,16 +241,7 @@ async def help_raid(event):
         await event.answer("ɴᴏᴏʙ ! ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ꜱᴛᴏʀᴍ ꜱᴘᴀᴍ ʙᴏᴛꜱ !! @KKEX_XD", cache_time=0, alert=True)
 
 
-@X1.on(events.CallbackQuery(pattern=r"extra"))
-@X2.on(events.CallbackQuery(pattern=r"extra"))
-@X3.on(events.CallbackQuery(pattern=r"extra"))
-@X4.on(events.CallbackQuery(pattern=r"extra"))
-@X5.on(events.CallbackQuery(pattern=r"extra"))
-@X6.on(events.CallbackQuery(pattern=r"extra"))
-@X7.on(events.CallbackQuery(pattern=r"extra"))
-@X8.on(events.CallbackQuery(pattern=r"extra"))
-@X9.on(events.CallbackQuery(pattern=r"extra"))
-@X10.on(events.CallbackQuery(pattern=r"extra"))
+@KEX1.on(events.CallbackQuery(pattern=r"extra"))
 async def help_extra(event):
     if event.query.user_id in SUDO_USERS:
         await event.edit(extra_msg,
